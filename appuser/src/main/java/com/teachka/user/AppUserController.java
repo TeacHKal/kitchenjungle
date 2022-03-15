ab32ea07ad071fd786d9242cc54142dfade421bc
TeacH <teach.inbox@gmail.com>
2022-03-15 18:49:38 +0100

    get all appUsers

--- appuser/src/main/java/com/teachka/user/AppUserController.java
+++ appuser/src/main/java/com/teachka/user/AppUserController.java
@@ -1,19 +1,24 @@
 package com.teachka.user;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("api/v1/users")
 public record AppUserController(AppUserService appuserService) {
 
+    @GetMapping
+    public List<AppUser> registerUser() {
+        log.info("get app users");
+        return appuserService.findAll();
+    }
+
     @PostMapping
-    public void registerUser(@RequestBody AppUserRegistrationRequest appuserRegistrationRequest) {
-        log.info("new user registration {}", appuserRegistrationRequest);
-        appuserService.registerUser(appuserRegistrationRequest);
+    public AppUser registerUser(@RequestBody AppUserRegistrationRequest appuserRegistrationRequest) {
+        log.info("new app_user registration {}", appuserRegistrationRequest);
+        return appuserService.registerUser(appuserRegistrationRequest);
     }
 }
--- appuser/src/main/java/com/teachka/user/AppUserService.java
+++ appuser/src/main/java/com/teachka/user/AppUserService.java
@@ -2,9 +2,17 @@ package com.teachka.user;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public record AppUserService(AppUserRepository repository) {
-    public void registerUser(AppUserRegistrationRequest request) {
+
+
+    public List<AppUser> findAll(){
+        return repository.findAll();
+    }
+
+    public AppUser registerUser(AppUserRegistrationRequest request) {
         AppUser appUser =  AppUser.builder()
                 .email(request.email())
                 .firstName(request.firstName())
@@ -15,5 +23,7 @@ public record AppUserService(AppUserRepository repository) {
         // TODO check if email is taken
         // TODO email confirmation
         repository.save(appUser);
+        return appUser;
     }
+
 }
