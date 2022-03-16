401889beef3f7c8150d1ea7a96992680bf14fb62
TeacH <teach.inbox@gmail.com>
2022-03-16 18:53:08 +0100

    refactore

--- appuser/src/main/java/com/teachka/user/AppUser.java
+++ appuser/src/main/java/com/teachka/user/AppUser.java
@@ -1,9 +1,6 @@
 package com.teachka.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -14,14 +11,9 @@ import javax.persistence.*;
 @Entity
 public class AppUser {
     @Id
-    @SequenceGenerator(
-            name = "app_user_id_sequence",
-            sequenceName = "app_user_id_sequence"
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "app_user_id_sequence"
-    )
+    @SequenceGenerator(name = "app_user_id_sequence", sequenceName = "app_user_id_sequence")
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "app_user_id_sequence")
     private Long id;
     @Column(unique = true)
     private String email;
